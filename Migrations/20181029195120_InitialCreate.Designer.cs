757dcd671f87c58db6c8ab74d826a3b3869c291b
Mohamed Mediouni <24752637+woachk@users.noreply.github.com>
2018-10-29 20:52:53 +0100

    Some data structure changes

--- Migrations/20181029195120_InitialCreate.Designer.cs
+++ Migrations/20181029195120_InitialCreate.Designer.cs
@@ -2,14 +2,16 @@
 using DiscordBackup;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DiscordBackup.Migrations
 {
     [DbContext(typeof(MessageContext))]
-    partial class MessageContextModelSnapshot : ModelSnapshot
+    [Migration("20181029195120_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
